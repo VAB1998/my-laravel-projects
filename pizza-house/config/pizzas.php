961bcd6d7c77a10fda67402eeb26a3a0158b80ed
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-01 14:30:53 +0100

    pizza-house. Added new Datas to Pizzas Database.

--- pizza-house/config/pizzas.php
+++ pizza-house/config/pizzas.php
@@ -5,5 +5,15 @@ return [
         'base' => 'cheese crust',
         'price' => 10
     ],
+    [
+        'type' => 'volcano', 
+        'base' => 'garlic crust',
+        'price' => 12
+    ],
+    [
+        'type' => 'veg supreme', 
+        'base' => 'thin and crispy',
+        'price' => 11
+    ],
 ]
 ?>
